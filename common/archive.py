822051c9278e76deee9ab8d4da96c7771c5d4a9f
srav <109066452+srav-ku@users.noreply.github.com>
2026-01-09 17:09:04 +0530

    Update archive.py

--- common/archive.py
+++ common/archive.py
@@ -5,24 +5,28 @@ ARCHIVE_ACCESS_KEY = os.environ["ARCHIVE_ACCESS_KEY"]
 ARCHIVE_SECRET_KEY = os.environ["ARCHIVE_SECRET_KEY"]
 
 def upload_file(file_path, identifier):
+    filename = os.path.basename(file_path)
+
     metadata = {
-        "title": os.path.basename(file_path).rsplit(".", 1)[0],
         "mediatype": "movies",
+        "title": filename.rsplit(".", 1)[0],
     }
+
     try:
         result = upload(
             identifier=identifier,
-            files=file_path,
+            files={filename: file_path},  # IMPORTANT: no overwrite
             metadata=metadata,
             access_key=ARCHIVE_ACCESS_KEY,
             secret_key=ARCHIVE_SECRET_KEY,
             retries=5,
             retries_sleep=2,
-            delete=False
+            delete=False,
         )
-        if result[0].status_code == 200:
+
+        if result and result[0].status_code == 200:
             return True, ""
-        else:
-            return False, str(result)
+        return False, str(result)
+
     except Exception as e:
         return False, str(e)
